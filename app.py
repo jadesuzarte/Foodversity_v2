d90754041ed2f22d90198ba02f5f9ef1c8a3816a
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-02-11 11:13:32 +0000

    Update app.py
    * Created routes.
    * Set configuration of SQLALCHEMY_TRACK_MODIFICATIONS to F

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) #initialized the database
 
 class Users(db.Model):
@@ -15,3 +16,14 @@ class Users(db.Model):
 
      def __repr__(self):
          return '<Task %r>' % self.id #EXPLAIN THIS
+
+@app.route('/')
+def home():
+    return render_template("homepage.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
