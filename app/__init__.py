325680cc97eaf5e626f452366beea6aeb92c2062
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-02-12 15:49:42 +0000

    Provided a secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = os.getenv("secretkey")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) #initialized the database
